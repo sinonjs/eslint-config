ef0f2341f673a7383310258904730c1c34287143
Morgan Roderick <morgan.roderick@zalando.de>
2018-10-08 23:02:56 +0200

    Enable more rules in 'variables'

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ const strictMode = {
 const variables = {
     "init-declarations": "off",
     "no-delete-var": "error",
-    "no-label-var": "off",
+    "no-label-var": "error",
     "no-restricted-globals": "off",
     "no-shadow": "error",
     "no-shadow-restricted-names": "error",
